2ed190a53c04ca58878d7ddbfcb963ea69a79d8c
mohammadreza2003-af <mohammadreza2003.af@gmail.com>
2025-07-17 09:21:11 +0430

    feat(provider) create provider

---
+++ src/providers/provider.tsx
@@ -0,0 +1,21 @@
+import { FC, ReactNode } from "react";
+import { ThemeProvider } from "next-themes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+type ProviderProps = {
+  children: ReactNode;
+};
+
+const Provider: FC<ProviderProps> = ({ children }) => {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" enableSystem>
+        {children}
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+};
+
+export default Provider;
