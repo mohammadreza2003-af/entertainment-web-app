3e2805674403c20508f4515e5889d6679b8de47e
mohammadreza2003-af <mohammadreza2003.af@gmail.com>
2025-07-30 11:38:51 -0700

    feat: create sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "@/providers/provider";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +30,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Provider>{children}</Provider>
+        <SidebarProvider>
+          <Provider>
+            <AppSidebar />
+            <main>
+              <SidebarTrigger />
+              {children}
+            </main>
+          </Provider>
+        </SidebarProvider>
       </body>
     </html>
   );
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,90 @@
+"use client";
+import {
+  LayoutDashboardIcon,
+  PopcornIcon,
+  ClapperboardIcon,
+} from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+
+const items = [
+  {
+    title: "home",
+    url: "/",
+    icon: LayoutDashboardIcon,
+  },
+  {
+    title: "movies",
+    url: "/movies",
+    icon: ClapperboardIcon,
+  },
+  {
+    title: "series",
+    url: "/series",
+    icon: PopcornIcon,
+  },
+];
+
+export function AppSidebar() {
+  const [activeItem, setActiveItem] = useState<string | null>("home");
+
+  console.log(activeItem);
+
+  return (
+    <Sidebar
+      variant="floating"
+      className="rounded-full h-[90vh] my-auto left-4"
+    >
+      <SidebarHeader className="flex items-center justify-center h-24">
+        <Link href="/">
+          <Image src="/images/logo.svg" alt="logo" width={32} height={32} />
+        </Link>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup className="h-full">
+          <SidebarGroupContent className="h-full">
+            <SidebarMenu className="flex flex-col space-y-6 items-center justify-start h-full">
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton
+                    onClick={() => setActiveItem(item.title)}
+                    asChild
+                    className="hover:bg-transparent"
+                  >
+                    <Link href={item.url}>
+                      <div className="w-6 h-6">
+                        <item.icon
+                          className={`w-full h-full  hover:text-red-500 transition duration-200 ease-in ${
+                            activeItem === item.title
+                              ? "text-white"
+                              : "text-gray-500"
+                          }`}
+                        />
+                      </div>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter className="flex flex-col items-center justify-center h-16 bg-gray-800 text-white">
+        <h2 className="text-center">Profile</h2>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
